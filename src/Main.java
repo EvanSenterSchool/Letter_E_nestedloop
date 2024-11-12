657d543c222e570ae6c70ff9f1770086551836e7
614925 <senter.614925@swl.k12.oh.us>
2024-11-12 08:54:20 -0500

    hi

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 17; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= 2; i++) {
+            for (int j = 1; j <= 6; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 12; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= 2; i++) {
+            for (int j = 1; j <= 6; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 17; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
